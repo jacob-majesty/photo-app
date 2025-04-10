2b252a7b32e0f76a4ef8ef971528832cdbdce38d
dev-majesty <dev.majesty.code@gmail.com>
2025-04-10 00:06:49 -0300

    Initial commit - account management

--- photoAccountManagement/src/main/java/com/majesty/photoAccountManagement/PhotoAccountManagementApplication.java
+++ photoAccountManagement/src/main/java/com/majesty/photoAccountManagement/PhotoAccountManagementApplication.java
@@ -1,4 +1,4 @@
-package com.majesty.photoAppApiUsers;
+package com.majesty.photoAccountManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PhotoAppApiUsersApplication {
+public class PhotoAccountManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+		SpringApplication.run(PhotoAccountManagementApplication.class, args);
 	}
 
 }
