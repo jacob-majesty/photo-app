79f41535a0b337beca9a36f4fd43367494087d24
dev-majesty <dev.majesty.code@gmail.com>
2025-04-10 00:06:08 -0300

    add ui controller - users

---
+++ photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/ui/controllers/UsersController.java
@@ -0,0 +1,10 @@
+package com.majesty.photoAppApiUsers.ui.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+}
