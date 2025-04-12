a82d6ba65dc7e490908c7b91854ebc2df0d1e31f
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-12 16:41:55 -0300

    add user controller

--- photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/ui/controllers/UsersController.java
+++ photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/ui/controllers/UsersController.java
@@ -1,10 +1,47 @@
 package com.majesty.photoAppApiUsers.ui.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.majesty.photoAppApiUsers.service.UsersService;
+import com.majesty.photoAppApiUsers.shared.UserDto;
+import com.majesty.photoAppApiUsers.ui.model.CreateUserRequestModel;
+import com.majesty.photoAppApiUsers.ui.model.CreateUserResponseModel;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    UsersService usersService;
+
+    @GetMapping("/status/check")
+    public String status()
+    {
+        return "Working on port " + env.getProperty("local.server.port");
+    }
+
+
+    @PostMapping
+    public ResponseEntity<CreateUserResponseModel> createUser(@RequestBody CreateUserRequestModel userDetails)
+    {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+
+        UserDto createdUser = usersService.createUser(userDto);
+
+        CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
+    }
 
 }
