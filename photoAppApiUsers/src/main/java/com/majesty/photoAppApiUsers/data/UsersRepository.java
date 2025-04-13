0f1bfc183d7bbc233cfba82d260c2208086bde70
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-13 00:16:04 -0300

    add feature to user repo

--- photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/data/UsersRepository.java
+++ photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/data/UsersRepository.java
@@ -3,4 +3,5 @@ package com.majesty.photoAppApiUsers.data;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
 }
