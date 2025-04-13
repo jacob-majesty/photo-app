642448ff109734f7a625438626403d861969a1fb
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-13 00:14:45 -0300

    add web security

--- photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/security/WebSecurity.java
+++ photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/security/WebSecurity.java
@@ -1,11 +1,16 @@
 package com.majesty.photoAppApiUsers.security;
 
+import com.majesty.photoAppApiUsers.service.UsersService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -13,28 +18,46 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
-    private Environment environment;
 
-    public WebSecurity(Environment environment) {
+    private final Environment environment;
+    private final UsersService usersService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebSecurity(Environment environment, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.environment = environment;
+        this.usersService = usersService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        http.csrf((csrf) -> csrf.disable());
+        // Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.userDetailsService(usersService)
+                .passwordEncoder(bCryptPasswordEncoder);
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        // Create AuthenticationFilter
+        AuthenticationFilter authenticationFilter =
+                new AuthenticationFilter(usersService, environment, authenticationManager);
+        authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         http.authorizeHttpRequests((authz) -> authz
-                        .requestMatchers(new AntPathRequestMatcher("/users/**")).access(
-                                new WebExpressionAuthorizationManager("hasIpAddress('"+environment.getProperty("gateway.ip")+"')"))
+                        .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll())
+                .addFilter(authenticationFilter)
+                .authenticationManager(authenticationManager)
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
-
         return http.build();
 
     }
-
 }
