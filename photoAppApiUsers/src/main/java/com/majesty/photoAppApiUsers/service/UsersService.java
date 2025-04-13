10d22385831511caba631b9090edfaa6d964701b
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-13 00:15:21 -0300

    add features to user service

--- photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/service/UsersService.java
+++ photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/service/UsersService.java
@@ -1,7 +1,10 @@
 package com.majesty.photoAppApiUsers.service;
 
 import com.majesty.photoAppApiUsers.shared.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
     UserDto createUser(UserDto userDetails);
+
+    UserDto getUserDetailsByEmail(String email);
 }
