78db1b8ac76d5b4acc73723b17f5abe02a868e1c
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-13 20:49:14 -0300

    Initial commit - photo album

---
+++ photoApiAlbums/src/test/java/com/majesty/photoApiAlbums/PhotoApiAlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majesty.photoApiAlbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoApiAlbumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
