6fd3dac5b2dbc2ae55fc0b07cbc8c8987638d8e0
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-13 01:55:13 -0300

    Initial commit - config server

---
+++ photoApiConfigServer/src/test/java/com/majesty/photoApiConfigServer/PhotoApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majesty.photoApiConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
