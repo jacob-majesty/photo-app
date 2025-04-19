ae92feb7c26b954c5d968f33c1622a8bbdbe9233
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-18 23:06:52 -0300

    add new get method

--- photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/ui/controllers/UsersController.java
+++ photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/ui/controllers/UsersController.java
@@ -4,11 +4,13 @@ import com.majesty.photoAppApiUsers.service.UsersService;
 import com.majesty.photoAppApiUsers.shared.UserDto;
 import com.majesty.photoAppApiUsers.ui.model.CreateUserRequestModel;
 import com.majesty.photoAppApiUsers.ui.model.CreateUserResponseModel;
+import com.majesty.photoAppApiUsers.ui.model.UserResponseModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,12 +27,13 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status()
     {
-        return "Working on port " + env.getProperty("local.server.port")
-                + ", with token = " + env.getProperty("token.secret");
+        return "Working on port " + env.getProperty("local.server.port") + ", with token = " + env.getProperty("token.secret");
     }
 
-
-    @PostMapping
+    @PostMapping(
+            consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+            produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+    )
     public ResponseEntity<CreateUserResponseModel> createUser(@RequestBody CreateUserRequestModel userDetails)
     {
         ModelMapper modelMapper = new ModelMapper();
@@ -44,5 +47,15 @@ public class UsersController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
+
+    @GetMapping(value="/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<UserResponseModel> getUser(@PathVariable("userId") String userId) {
+
+        UserDto userDto = usersService.getUserByUserId(userId);
+        UserResponseModel returnValue = new ModelMapper().map(userDto, UserResponseModel.class);
+
+        return ResponseEntity.status(HttpStatus.OK).body(returnValue);
+    }
+
 
 }
