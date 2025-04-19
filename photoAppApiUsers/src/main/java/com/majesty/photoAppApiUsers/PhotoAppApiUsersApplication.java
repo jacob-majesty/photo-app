e1b30c67297e2f8897bf422e5ba7405a078fd02b
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-04-18 23:01:19 -0300

    add feign log in main

--- photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/PhotoAppApiUsersApplication.java
+++ photoAppApiUsers/src/main/java/com/majesty/photoAppApiUsers/PhotoAppApiUsersApplication.java
@@ -1,17 +1,22 @@
 package com.majesty.photoAppApiUsers;
 
+import com.majesty.photoAppApiUsers.shared.FeignErrorDecoder;
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
@@ -34,6 +39,20 @@ public class PhotoAppApiUsersApplication {
 	{
 		return new RestTemplate();
 	}
+
+	@Bean
+	@Profile("!production")
+	Logger.Level feignDefaultLoggerLevel()
+	{
+		return Logger.Level.FULL;
+	}
+
+	/*
+	@Bean
+	public FeignErrorDecoder getFeignErrorDecoder()
+	{
+		return new FeignErrorDecoder();
+	} */
 
 
 }
